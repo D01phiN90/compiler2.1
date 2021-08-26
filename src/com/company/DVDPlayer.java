638fb73ba172e0018c5f228bd72e622c254a67da
D01phiN90 <denisdreidt@gmail.com>
2021-08-26 06:39:25 +0300

    Initial commit

---
+++ src/com/company/DVDPlayer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void playDVD(){
+        System.out.println("Включение проигрывателя DVD");
+    }
+
+    void recordDVD(){
+        System.out.println("идет запись DVD");
+    }
+}
